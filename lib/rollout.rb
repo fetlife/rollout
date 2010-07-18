8d2307ee6c85202b07a4b34b54c7349c590942b1
James Golick <james@giraffesoft.ca>
2010-07-17 20:11:48 -0700

    basic group admission

---
+++ lib/rollout.rb
@@ -0,0 +1,23 @@
+class Rollout
+  def initialize(redis)
+    @redis  = redis
+    @groups = {"all" => lambda { |user| true }}
+  end
+
+  def activate(feature, group)
+    @redis.sadd(key(feature), group)
+  end
+
+  def define_group(group, &block)
+    @groups[group.to_s] = block
+  end
+
+  def active?(feature, user)
+    @redis.smembers(key(feature)).any? { |group| @groups[group].call(user) }
+  end
+
+  private
+    def key(name)
+      "feature:#{name}"
+    end
+end
--- spec/rollout_spec.rb
+++ spec/rollout_spec.rb
@@ -1,7 +1,33 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Rollout" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before do
+    @redis   = Redis.new
+    @rollout = Rollout.new(@redis)
+  end
+
+  describe "when a group is activated" do
+    before do
+      @rollout.define_group(:fivesonly) { |user| user.id == 5 }
+      @rollout.activate(:chat, :fivesonly)
+    end
+
+    it "the feature is active for users for which the block evaluates to true" do
+      @rollout.should be_active(:chat, stub(:id => 5))
+    end
+
+    it "is not active for users for which the block evaluates to false" do
+      @rollout.should_not be_active(:chat, stub(:id => 1))
+    end
+  end
+
+  describe "the default all group" do
+    before do
+      @rollout.activate(:chat, :all)
+    end
+
+    it "evaluates to true no matter what" do
+      @rollout.should be_active(:chat, nil)
+    end
   end
 end
