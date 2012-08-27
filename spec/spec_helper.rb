23599a380aa9cae395875813e1732ae4cb1d0cde
Piotr Usewicz <piotr@layer22.com>
2012-08-27 18:30:32 +0100

    Update to newer RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rollout'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
 require 'bourne'
 require 'redis'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.mock_with :mocha
   config.before { Redis.new.flushdb }
 end
