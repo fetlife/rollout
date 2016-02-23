4763615dfb6a3f4814919d66f845e47a38583633
Rene Klacan <rene.klacan@gmail.com>
2016-02-23 11:37:20 +0100

    Use fakeredis for dev and real one for CI
    to avoid wipe of whole DB 1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require "rollout"
 require "rspec"
-require "redis"
+require(p(ENV["TRAVIS_CI"] ? "redis" : "fakeredis"))
 require "codeclimate-test-reporter"
 
 SimpleCov.start do
