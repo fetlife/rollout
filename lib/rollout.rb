788d99716eb49810095e09a0d681d8f11717d576
Shane O'Grady <shane@ogrady.ie>
2017-04-07 17:14:56 -0300

    Replace Fixnum with Integer

--- lib/rollout.rb
+++ lib/rollout.rb
@@ -76,7 +76,7 @@ class Rollout
 
     private
       def user_id(user)
-        if user.is_a?(Fixnum) || user.is_a?(String)
+        if user.is_a?(Integer) || user.is_a?(String)
           user.to_s
         else
           user.send(id_user_by).to_s
