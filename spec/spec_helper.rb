28e597428d8f8494340271d30d8af81a5c6703b2
fatkodima <fatkodima123@gmail.com>
2023-04-22 21:51:31 +0300

    Support `redis-rb` 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,8 @@ require 'bundler/setup'
 require 'redis'
 require 'rollout'
 
-Redis.current = Redis.new(
+Redis.raise_deprecations = true
+$redis = Redis.new(
   host: ENV.fetch('REDIS_HOST', '127.0.0.1'),
   port: ENV.fetch('REDIS_PORT', '6379'),
   db: ENV.fetch('REDIS_DB', '7'),
@@ -23,5 +24,5 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.before { Redis.current.flushdb }
+  config.before { $redis.flushdb }
 end
